ab8bad697c1864f9fd9f07ef260b17f4df65406d
Akshat Jain <akshat2002@berkeley.edu>
2020-12-29 18:37:38 -0700

    User Authentication

---
+++ Node_User_Authentication/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://akshat02:akshat02@todolist-app.qqrq6.mongodb.net/test-database?retryWrites=true&w=majority",
+};
---
+++ Node_User_Authentication/routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { ensureAuthenticated } = require("../config/auth");
+
+router.get("/", (req, res) => res.render("welcome.ejs"));
+
+// Dashboard
+router.get("/dashboard", ensureAuthenticated, (req, res) =>
+  res.render("dashboard")
+);
+
+module.exports = router;
